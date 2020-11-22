6fb6be3591e19bf031550eff9bd4a1ef8b23873a
Fabio A. Castiblanco <fcastibl@purdue.edu>
2020-11-21 19:54:24 -0500

    Added Drawers and Appbars to GUI

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 import React, { useCallback, useState } from "react";
+import clsx from 'clsx';
 import { useOvermind } from "../overmind";
 import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import MapComponent from "./Map";
-import DrawerComponent from "./Drawer";
+import LeftDrawerComponent from "./LeftDrawer";
+import BottomDrawerComponent from "./BottomDrawer"
 import CssBaseline from "@material-ui/core/CssBaseline";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -12,21 +16,29 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     content: {
       flexGrow: 1,
-      backgroundColor: theme.palette.background.default,
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
     },
+    toolbar: theme.mixins.toolbar,
   })
 );
 
 const App = () => {
   const { state, actions } = useOvermind();
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <DrawerComponent />
+      <LeftDrawerComponent />
       <main className={classes.content}>
-        <MapComponent />
+        <div className={classes.toolbar} />
+          <MapComponent />
       </main>
+      <BottomDrawerComponent />
     </div>
   );
 };
--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,45 +1,143 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
 import { useOvermind } from "../overmind";
-import React from "react";
-import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import Divider from "@material-ui/core/Divider";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from "@material-ui/icons/Dashboard";
-import Typography from "@material-ui/core/Typography";
+import MapComponent from "./Map";
+
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
+    root: {
+      display: 'flex',
+    },
+    appBar: {
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    hide: {
+      display: 'none',
+    },
     drawer: {
-      width: 240,
+      width: drawerWidth,
       flexShrink: 0,
     },
     drawerPaper: {
-      width: 240,
+      width: drawerWidth,
     },
-  })
+    drawerHeader: {
+      display: 'flex',
+      alignItems: 'center',
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: 'flex-end',
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  }),
 );
 
-const DrawerComponent = () => {
-  const { state, actions } = useOvermind();
-  const classes = useStyles();
-  return (
-    <Drawer
-      className={classes.drawer}
-      variant="permanent"
-      classes={{
-        paper: classes.drawerPaper,
-      }}
-      anchor="left"
-    >
-      <Typography variant="h4" component="h1" align="center">
-        ADAP
-      </Typography>
 
-      <Divider />
-      <List>
+const DrawerComponent = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const { state, actions } = useOvermind();
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography 
+            variant="h4" 
+            noWrap>
+            ADAP
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
         {["Field A", "Field B", "Field C"].map((text, index) => (
           <ListItem
             button
@@ -53,8 +151,19 @@ const DrawerComponent = () => {
           </ListItem>
         ))}
       </List>
-    </Drawer>
+        <Divider />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+          className={classes.content}
+          <MapComponent />
+      </main>
+    </div>
   );
-};
+}
 
 export default DrawerComponent;
