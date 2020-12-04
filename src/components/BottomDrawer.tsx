8ad4746ffc10d77c0d1ec35b2ad69c4c1c4f4f86
Tomohiro Arakawa <tarakawa@purdue.edu>
2020-12-04 12:04:56 +0900

    Make minor change to the bottom drawer

--- src/components/BottomDrawer.tsx
+++ src/components/BottomDrawer.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme, Theme, createStyles, withStyles } from '@material-ui/core/styles';
+import React from "react";
+import clsx from "clsx";
+import {
+  makeStyles,
+  useTheme,
+  Theme,
+  createStyles,
+  withStyles,
+} from "@material-ui/core/styles";
 import { useOvermind } from "../overmind";
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import LinearProgress, { LinearProgressProps } from '@material-ui/core/LinearProgress';
-import Box from '@material-ui/core/Box';
-import { selectField } from '../overmind/actions';
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import LinearProgress, {
+  LinearProgressProps,
+} from "@material-ui/core/LinearProgress";
+import Box from "@material-ui/core/Box";
+import { selectField } from "../overmind/actions";
 
 const drawerHeight = 280;
 
 const THEME = createMuiTheme({
-    palette: {
-      type: 'dark',
-    },
-  });
+  palette: {
+    type: "dark",
+  },
+});
 
 const BorderLinearProgress = withStyles((theme: Theme) =>
   createStyles({
@@ -31,71 +39,72 @@ const BorderLinearProgress = withStyles((theme: Theme) =>
       borderRadius: 5,
     },
     colorPrimary: {
-      backgroundColor: theme.palette.grey[theme.palette.type === 'light' ? 200 : 700],
+      backgroundColor:
+        theme.palette.grey[theme.palette.type === "light" ? 200 : 700],
     },
     bar: {
       borderRadius: 5,
-      backgroundColor: '#FFFFFF',
+      backgroundColor: "#FFFFFF",
     },
-  }),
+  })
 )(LinearProgress);
 
-function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
+function LinearProgressWithLabel(
+  props: LinearProgressProps & { value: number }
+) {
   return (
     <Box display="flex" alignItems="center">
       <Box width="100%" mr={1}>
         <BorderLinearProgress variant="determinate" {...props} />
       </Box>
       <Box minWidth={35}>
-        <Typography
-          variant="body2"
-          color="textPrimary">
-            {`${Math.round(props.value,)}%`}
+        <Typography variant="body2" color="textPrimary">
+          {`${Math.round(props.value)}%`}
         </Typography>
       </Box>
     </Box>
-    );
-  }
+  );
+}
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      display: 'flex',
+      display: "flex",
     },
     appBar: {
-      transition: theme.transitions.create(['margin', 'height'], {
+      transition: theme.transitions.create(["margin", "height"], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
       }),
-      top: 'auto',
+      top: "auto",
       bottom: 0,
-      alignItems: 'center',
+      alignItems: "center",
       zIndex: 3,
-      background: '#1082B8'
+      background: "#1082B8",
     },
     appBarShift: {
-      transition: theme.transitions.create(['margin', 'height'], {
+      transition: theme.transitions.create(["margin", "height"], {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
       zIndex: 3,
     },
     appBarProgress: {
-      transition: theme.transitions.create(['margin', 'height'], {
+      transition: theme.transitions.create(["margin", "height"], {
         easing: theme.transitions.easing.sharp,
         duration: theme.transitions.duration.leavingScreen,
       }),
       zIndex: 3,
-      alignItems: 'center',
+      alignItems: "center",
     },
     menuButton: {
       marginRight: theme.spacing(2),
     },
-    bottomBarButton:{
-      alignItems: 'center',
+    bottomBarButton: {
+      alignItems: "center",
     },
     hide: {
-      display: 'none',
+      display: "none",
     },
     drawer: {
       height: drawerHeight,
@@ -103,33 +112,31 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     drawerPaper: {
       height: drawerHeight,
-      background: '#1082B8',
-      alignItems: 'center',
-      width: '100%',
+      background: "#1082B8",
+      alignItems: "center",
+      width: "100%",
     },
-    drawerEmptyField:{
+    drawerEmptyField: {
       height: drawerHeight - 160,
-      background: '#1082B8',
-      alignItems: 'center',
+      background: "#1082B8",
+      alignItems: "center",
     },
     progressBar: {
-      width: '75%',
-      padding: theme.spacing(2,0,4),
-    }
-  }),
+      width: "75%",
+      padding: theme.spacing(2, 0, 4),
+    },
+  })
 );
 
-function showProgress(state){
-  
-  if(state.selectedField === null){
+function showProgress(state) {
+  if (state.selectedField === null) {
     return false;
-  }
-  else{
+  } else {
     return true;
   }
 }
 
-function truncate (num, places) {
+function truncate(num, places) {
   return Math.trunc(num * Math.pow(10, places)) / Math.pow(10, places);
 }
 
@@ -153,76 +160,91 @@ const BottomDrawerComponent = () => {
     <div className={classes.root}>
       <CssBaseline />
       <MuiThemeProvider theme={THEME}>
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open, [classes.appBarProgress]: showProgressBar
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.bottomBarButton, open  && classes.hide)}
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+            [classes.appBarProgress]: showProgressBar,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(classes.bottomBarButton, open && classes.hide)}
+            >
+              <ExpandLessIcon />
+            </IconButton>
+          </Toolbar>
+          <div
+            className={clsx(
+              !showProgressBar && classes.hide,
+              classes.progressBar
+            )}
           >
-            <ExpandLessIcon />
-          </IconButton>
-        </Toolbar>
-        <div className={clsx(!showProgressBar && classes.hide, classes.progressBar)}>
-          <LinearProgressWithLabel 
-            value={state.progress.percentage} />
-        </div>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor='bottom'
-        open={open}
-        classes={{
-          paper: clsx(classes.drawerPaper, !showProgressBar && classes.drawerEmptyField),
-        }}
-      >
-        <div className={classes.bottomBarButton}>
-          <IconButton onClick={handleDrawerClose}>
-            <ExpandMoreIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <Typography
-          variant='h6'
-          color="textSecondary"
-          style={{ fontWeight: 600 }}
-          gutterBottom>
-          {state.selectedField !== null? state.fields[state.selectedField] : "(No field has been selected)"}
-        </Typography>
-        <div className={clsx(!showProgressBar && classes.hide)}>
+            <LinearProgressWithLabel value={state.progress.percentage} />
+          </div>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="persistent"
+          anchor="bottom"
+          open={open}
+          classes={{
+            paper: clsx(
+              classes.drawerPaper,
+              !showProgressBar && classes.drawerEmptyField
+            ),
+          }}
+        >
+          <div className={classes.bottomBarButton}>
+            <IconButton onClick={handleDrawerClose}>
+              <ExpandMoreIcon />
+            </IconButton>
+          </div>
+          <Divider />
           <Typography
-            color="textSecondary">
-            Harvest Progress 
+            variant="h6"
+            color="textSecondary"
+            style={{ fontWeight: 600 }}
+            gutterBottom
+          >
+            {state.selectedField !== null
+              ? state.fields[state.selectedField]
+              : "(No field has been selected)"}
           </Typography>
-        </div>
-        <div className={clsx(!showProgressBar && classes.hide, classes.progressBar)}>
-          <LinearProgressWithLabel value={state.progress.percentage} />
-        </div>
-        <div className={clsx(!showProgressBar && classes.hide)}>
-          <Typography
-            color="textSecondary">
-            Harvested Area: {truncate(state.progress.acre, 2)} ac
-          </Typography>
-          <Typography
-            color="textSecondary">
-            Number of Fields Being Harvested: {state.statistics.numOfFieldsBeingHarvested}
-          </Typography>
-          <Typography
-            color="textSecondary">
-            Number of Fields Harvested: {state.statistics.numOfFieldsHarvested}
-          </Typography>
-        </div>
+          <div className={clsx(!showProgressBar && classes.hide)}>
+            <Typography color="textSecondary">Harvest Progress</Typography>
+          </div>
+          <div
+            className={clsx(
+              !showProgressBar && classes.hide,
+              classes.progressBar
+            )}
+          >
+            <LinearProgressWithLabel value={state.progress.percentage} />
+          </div>
+          <div className={clsx(!showProgressBar && classes.hide)}>
+            <Typography color="textSecondary">
+              Harvested Area: {truncate(state.progress.acre, 2)} ac
+            </Typography>
+            <Typography color="textSecondary">
+              Number of Fields Being Harvested:{" "}
+              {state.statistics.numOfFieldsBeingHarvested}
+            </Typography>
+            <Typography color="textSecondary">
+              Number of Fields Harvested:{" "}
+              {state.statistics.numOfFieldsHarvested}
+            </Typography>
+            <Typography color="textSecondary">
+              Overall Progress: {Math.round(state.statistics.progress * 100)}
+            </Typography>
+          </div>
         </Drawer>
       </MuiThemeProvider>
     </div>
   );
-}
+};
 
 export default BottomDrawerComponent;
